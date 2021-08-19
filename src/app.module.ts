62e07c64a02a4e5f34fc5ff76e06baf08453f025
samdtech <opeyemisamuel222@gmal.com>
2021-08-19 11:43:30 +0100

    applying global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./users/user.entity";
-import { Report } from "./reports/report.entity";
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: "sqlite",
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true
-  })],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
