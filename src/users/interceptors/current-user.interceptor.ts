074e62aa336d212d3e9678cfb672e413cb6640ab
samdtech <opeyemisamuel222@gmal.com>
2021-08-19 11:13:48 +0100

    creating e2e test

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -11,19 +11,16 @@ import { Observable } from 'rxjs';
 export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
 
- async intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ) {
-  const request = context.switchToHttp().getRequest()
+  async intercept(context: ExecutionContext, next: CallHandler<any>) {
+    const request = context.switchToHttp().getRequest();
 
-    const { userId } = request?.session
-    if(userId){
-  const user = await this.usersService.findOne(userId)
+    const userId = request?.session?.userId;
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
 
-      request.currentUser = user
+      request.currentUser = user;
     }
 
-    return next.handle()
+    return next.handle();
   }
 }
